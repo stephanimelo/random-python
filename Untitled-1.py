f28e25045d8b18d941342b8777a94791c4dd4aa8
Stephani <stephani.melo22@gmail.com>
2024-10-12 02:09:59 -0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,5 @@
+lado = float(input("Escreva o lado do quadrado"))
+
+area = lado * lado
+
+print(f'A área do quadrado é: {area}')
---
+++ Untitled-2.py
@@ -0,0 +1,7 @@
+salario = float(input("Insira o salário do funcionário: "))
+
+reajuste = salario * (15 / 100)
+
+salarioReajustado = salario + reajuste
+
+print(f'O salário reajustado é: {salarioReajustado}')
